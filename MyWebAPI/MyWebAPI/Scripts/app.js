36fbfcd5201a489cc6e1cc9b851991f7c00d3fbb
srichatala <sri2vas@gmail.com>
2015-08-20 14:46:37 -0400

    front end edit and delete options added

--- MyWebAPI/MyWebAPI/Scripts/app.js
+++ MyWebAPI/MyWebAPI/Scripts/app.js
@@ -3,10 +3,14 @@
 app.controller("myWebAPICtrl", function ($scope,$http) {
     $scope.name = "Hello World, Welcome to Web API Application";
 
-    $http.get("/api/Students")
-       .success(function (response) {
-           $scope.data = response;
-       });
+    $scope.renderStudentInfo = function (response) {
+        $scope.data = response;
+    };
+    $scope.StudentInfo = function () {
+        $http.get("/api/Students")
+        .success($scope.renderStudentInfo);
+    }
+    $scope.StudentInfo();
     $scope.add = function (student) {
         console.log(student);
         $http.post("/api/Students/", student)
