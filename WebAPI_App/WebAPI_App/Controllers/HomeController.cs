b30f57e990615bf83294f60ebc23158faea2e0ee
srichatala <sri2vas@gmail.com>
2015-08-20 00:40:30 -0400

    layout view modified

--- WebAPI_App/WebAPI_App/Controllers/HomeController.cs
+++ WebAPI_App/WebAPI_App/Controllers/HomeController.cs
@@ -6,12 +6,12 @@ using System.Web.Mvc;
 
 namespace WebAPI_App.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
         }
     }
