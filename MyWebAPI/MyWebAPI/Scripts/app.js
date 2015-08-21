f8ad61de98c54dcd266a09ca8f149b25f7001a06
srichatala <sri2vas@gmail.com>
2015-08-21 01:17:19 -0400

    delete option append to index.html

--- MyWebAPI/MyWebAPI/Scripts/app.js
+++ MyWebAPI/MyWebAPI/Scripts/app.js
@@ -3,20 +3,25 @@
 app.controller("myWebAPICtrl", function ($scope,$http) {
     $scope.name = "Hello World, Welcome to Web API Application";
 
-    $scope.renderStudentInfo = function (response) {
-        $scope.data = response;
-    };
     $scope.StudentInfo = function () {
-        $http.get("/api/Students")
-        .success($scope.renderStudentInfo);
-    }
-    $scope.StudentInfo();
+        $http.get("api/Students")
+        .success(function (response) {
+            $scope.StData = response;
+        });
+    };
+    
     $scope.add = function (student) {
-        console.log(student);
         $http.post("/api/Students/", student)
             .success(function (response) {
-                $scope.data = response;
+                $scope.StData = response;
             });
 
-    }
+    };
+    $scope.remove = function (StudentID) {
+        $http.delete("/api/Students/" + StudentID)
+            .success(function (response) {
+                $scope.StData = response;
+            });
+    };
+    $scope.StudentInfo();
 });
