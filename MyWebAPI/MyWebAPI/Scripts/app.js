64c09d61d77158a2bb7c32c36e16ca0444bd5fae
srichatala <sri2vas@gmail.com>
2015-08-21 01:58:56 -0400

    end user can select any record to view individual record and update the record as well.

--- MyWebAPI/MyWebAPI/Scripts/app.js
+++ MyWebAPI/MyWebAPI/Scripts/app.js
@@ -3,25 +3,42 @@
 app.controller("myWebAPICtrl", function ($scope,$http) {
     $scope.name = "Hello World, Welcome to Web API Application";
 
-    $scope.StudentInfo = function () {
-        $http.get("api/Students")
-        .success(function (response) {
-            $scope.StData = response;
-        });
+    $scope.renderStudentModels = function (response) {
+        $scope.StData = response;
     };
-    
+
+    $scope.StudentInfo = function () {
+        $http.get("api/Students/")
+            .success($scope.renderStudentModels);
+    }
+
+    $scope.StudentInfo();
+
     $scope.add = function (student) {
         $http.post("/api/Students/", student)
             .success(function (response) {
-                $scope.StData = response;
+                $scope.StudentInfo();
             });
 
     };
     $scope.remove = function (StudentID) {
         $http.delete("/api/Students/" + StudentID)
             .success(function (response) {
-                $scope.StData = response;
+                $scope.StudentInfo();
             });
     };
-    $scope.StudentInfo();
+
+    $scope.select = function (StudentID) {
+        $http.get("api/Students/" + StudentID)
+            .success(function (response) {
+                $scope.student = response;
+            });
+    };
+
+    $scope.update = function (student) {
+        $http.put("/api/Students/" + student.StudentID, student)
+            .success(function (response) {
+                $scope.StudentInfo();
+            });
+    };
 });
