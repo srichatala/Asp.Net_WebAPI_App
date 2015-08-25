02b0ed4b651ae24f6a6cab7dd03d81cf1ee91297
srichatala <sri2vas@gmail.com>
2015-08-25 01:48:23 -0400

    Angularjs Routeprovider added for routing

--- SinglePageApp/SinglePageApp/Scripts/Controllers/ProfileCtrl.js
+++ SinglePageApp/SinglePageApp/Scripts/Controllers/ProfileCtrl.js
@@ -1,12 +1,11 @@
-﻿var app = angular.module("mySPA", [])
-app.controller("mySPACtrl", function ($scope,$http) {
+﻿app.controller("ProfileCtrl", function ($scope, $http) {
     $scope.name = "Hello world, Welcome to Single Page Application";
 
     $scope.renderProfileInfo = function (response) {
         console.log(response);
         $scope.ProfileData = response;
     };
-     
+
     $scope.ProfileInfo = function () {
         $http.get("api/Profiles")
         .success($scope.renderProfileInfo);
@@ -16,7 +15,7 @@ app.controller("mySPACtrl", function ($scope,$http) {
 
 
     $scope.add = function (Profile) {
-        $http.post("api/Profiles",Profile)
+        $http.post("api/Profiles", Profile)
         .success(function (response) {
             $scope.ProfileInfo();
         });
---
+++ SinglePageApp/SinglePageApp/Scripts/app.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module("mySPA", ["ngRoute"]);
+
+app.config(['$routeProvider', function ($routeProvider, $httpprovider) {
+    $routeProvider.
+        when('/Home', {
+            templateUrl: 'View/Home.html'
+        }).
+        when('/Profile', {
+            templateUrl: 'View/Profile.html',
+            controller: 'ProfileCtrl'
+        }).
+        when('/Article', {
+            templateUrl: 'View/Article.html'
+        }).
+        otherwise({
+            redirectTo:'/'
+        });
+}]);
